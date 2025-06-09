4fc8cc13ba09615fcc7ea6c24e05cbe22cee84fb
Gaurav <gauravkrdas19@gmail.com>
2025-06-09 22:52:30 +0530

    added fonts and colors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='w-full h-full bg-zinc-300'>
+    <div className='w-full h-full bg-platinum'>
       Hello
     </div>
   )
