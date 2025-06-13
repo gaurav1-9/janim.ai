1d6f140d63e6e293694c8740cfc4f6c571c638b5
Gaurav <gauravkrdas19@gmail.com>
2025-06-13 23:08:00 +0530

    mobile biew category componet break down

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useEffect, useRef } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import GenerateQuiz from './pages/GenerateQuiz'
 import Navbar from './components/Navbar'
 
 const App = () => {
+  const topRef = useRef(null);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    topRef.current?.scrollIntoView({ behavior: "auto" });
+  }, [pathname]);
+
+
   return (
-    <div className='w-full bg-seaSalt'>
+    <div className='w-full bg-seaSalt' ref={topRef}>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
--- client/src/components/GenerateQuiz-Components/QuizCategory.jsx
+++ client/src/components/GenerateQuiz-Components/QuizCategory.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import Buttons from '../Buttons'
-import { FaArrowRightLong } from "react-icons/fa6";
+import MobileViewCategory from './MobileViewCategory';
 
 const QuizCategory = ({ category, changeCategory }) => {
     return (
@@ -9,47 +8,7 @@ const QuizCategory = ({ category, changeCategory }) => {
             <div className="hidden lg:flex"></div>
 
             {/* Mobile Layout */}
-            <div className="flex flex-col lg:hidden">
-                <div className="mt-11 flex justify-center items-center w-full gap-11 md:gap-21 min-[425px]:gap-17 min-[375px]:gap-13 min-[425px]:pt-1 md:pt-4">
-                    <div
-                        className={`flex flex-col justify-center items-center p-2 w-17 ${(category.category === 'I') ? 'bg-pistachio' : 'bg-chineseViolet'} text-ivory rounded-lg`}
-                        onClick={() => changeCategory(0)}
-                    >
-                        <p className='text-xs'>Category</p>
-                        <p className='text-3xl font-semibold'>I</p>
-                    </div>
-                    <div
-                        className={`flex flex-col justify-center items-center p-2 w-17 ${(category.category === 'II') ? 'bg-pistachio' : 'bg-chineseViolet'} text-ivory rounded-lg`}
-                        onClick={() => changeCategory(1)}
-                    >
-                        <p className='text-xs'>Category</p>
-                        <p className='text-3xl font-semibold'>II</p>
-                    </div>
-                    <div
-                        className={`flex flex-col justify-center items-center p-2 w-17 ${(category.category === 'III') ? 'bg-pistachio' : 'bg-chineseViolet'} text-ivory rounded-lg`}
-                        onClick={() => changeCategory(2)}
-                    >
-                        <p className='text-xs'>Category</p>
-                        <p className='text-3xl font-semibold'>III</p>
-                    </div>
-                </div>
-                <p className='text-sm text-chineseViolet px-8 md:px-20 pt-4 pb-1'>Question Category...</p>
-                <div className="px-8 md:px-20 text-sm text-chineseViolet">
-                    <div className="flex justify-between items-center border-2 rounded-xl px-4 py-2 mb-2">
-                        <div>
-                            <p className='text-xl leading-4 font-semibold pt-2'>{category.questions}</p>
-                            <p>{category.time}</p>
-                        </div>
-                        <div className='bg-chineseViolet text-ivory text-sm py-2 px-3 text-center rounded-lg font-semibold'>
-                            <p>+{category.points}</p>
-                            <p>points</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="px-8 md:px-20 lg:hidden mb-5">
-                    <Buttons bgColor='bg-chineseViolet hover:bg-chineseViolet/90 hover:border-chineseViolet/0' text='Start the Quiz' goTo='/generate' icon={<FaArrowRightLong />} textColor='text-ivory' width='w-full' otherResponsiveStyles='py-2 text-lg' />
-                </div>
-            </div>
+            <MobileViewCategory category={category} changeCategory={changeCategory}/>
         </div>
     )
 }
