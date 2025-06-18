7f2c02018e213cf02ad0291002d1a4efa54924d7
Gaurav <gauravkrdas19@gmail.com>
2025-06-18 20:58:02 +0530

    mongodb setup and UserSchema defined

---
+++ server/Models/UserSchema.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 3,
+        trim: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        minLength: 3,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 3,
+        trim: true,
+    },
+    gender: {
+        type: String,
+        enum: ['male', 'female'],
+        required: true,
+    },
+    levelPoints: {
+        type: Number,
+        default: 0,
+    }
+})
+
+module.exports = mongoose.model("users", userSchema)
