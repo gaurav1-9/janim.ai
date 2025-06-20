23d944dfefe16c43d637a67105a39a056d0cf0a3
Gaurav <gauravkrdas19@gmail.com>
2025-06-21 01:20:49 +0530

    added server homepage

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config()
 
-const {AuthRoute} = require("./Routes/auth.routes")
+const { AuthRoute } = require("./Routes/auth.routes")
 const UserRoute = require("./Routes/user.routes")
 const QuizRoute = require("./Routes/quiz.routes")
 
@@ -13,11 +13,13 @@ const connectToDB = require("./config/config_DB")
 connectToDB();
 
 const app = express()
+app.set("view engine", "ejs")
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"));
 app.use(morgan("dev"))
 
-
+app.get("/", (req, res) => { res.render("index") })
 app.use("/api/auth", AuthRoute)
 app.use("/api/users", UserRoute)
 app.use("/api/quiz", QuizRoute)
